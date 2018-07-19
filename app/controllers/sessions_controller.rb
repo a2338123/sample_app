de8cc74d4bc17eda13bff47481026dbc27f0cca6
yu aokai <1251647820@qq.com>
2018-07-19 21:20:55 +0800

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-	if @user && @user.authenticate(params[:session][:password])
-	  log_in @user
-	  params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-	  redirect_back_or @user
+    user = User.find_by(email: params[:session][:email].downcase)
+	if user && user.authenticate(params[:session][:password])
+	  if user.activated?
+	  log_in user
+	  params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+	  redirect_back_or user
       #登入用户，然后重定向到用户的资料页面
 	  else
-      flash.now[:danger] = "Invalid email/password combination" 
-      render 'new'
+	  message = "Account not activated. "
+	  message += "Check your email for the activation link"
+      flash[:warning] = message 
+	  redirect_to root_url
+      end
+	else
+	  flash.now[:danger] = "Invalid email/password combination"
+	  render 'new'
     end
   end
 
