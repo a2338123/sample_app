df471f6de8c227f86c266b4f3cab17025147c4cb
yu aokai <1251647820@qq.com>
2018-07-19 14:58:00 +0800

    Finish user edit, update, index, and destroy acitons

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  
+
+
+	
+end
---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  def setup  
+    @user = users(:haha)
+  end
+
+
+
+	
+	
+
+
+
+ 
+end
