63ad9df29b3b2e627b33a68dd6f118877bd25158
yu aokai <1251647820@qq.com>
2018-07-20 23:15:15 +0800

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:haha)
+	@other_user = users(:archer)
+  end
  
   test "should get new" do
     get signup_path
 	assert_response :success
   end
+
 end
